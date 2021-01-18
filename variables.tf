8d799c62ad0adad91821d492d9daf11eaf25bb98
Dave Gallant <davegallant@gmail.com>
2021-01-17 23:56:33 -0500

    Bump minecraft from 1.12.0 to 1.13.0 (#22)
    * Bump minecraft from 1.12.0 to 1.13.0
    
    * Deploy only on master

--- variables.tf
+++ variables.tf
@@ -27,7 +27,7 @@ variable "seed" {
 variable "minecraft_version" {
   description = "The tag/version of the minecraft docker image. https://hub.docker.com/r/itzg/minecraft-server/tags"
   type        = string
-  default     = "1.12.0-multiarch"
+  default     = "1.13.0-multiarch"
 }
 
 variable "difficulty" {
