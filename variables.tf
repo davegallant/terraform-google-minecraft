80af32b3db07ea92b22fa218324cf07e2dfcf224
Dave Gallant <davegallant@gmail.com>
2021-02-28 18:07:56 -0500

    Update Minecraft image from 2021.5.1 to 2021.6.0

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "seed" {
 variable "minecraft_version" {
   description = "The tag/version of the minecraft docker image. https://hub.docker.com/r/itzg/minecraft-server/tags"
   type        = string
-  default     = "2021.5.1-multiarch"
+  default     = "2021.6.0-multiarch"
 }
 
 variable "difficulty" {
